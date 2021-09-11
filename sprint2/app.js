a8778efe7d13bb51036faaa78c807decd66cc511
juampa-arguello <jpablo.arguello@gmail.com>
2021-09-10 23:46:49 -0300

    Agregando home y login

--- sprint2/app.js
+++ sprint2/app.js
@@ -8,8 +8,8 @@ app.use(express.static('public'));
 app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')))
 app.get('/producto',(req,res) => res.sendFile(path.join(__dirname,'views','product.html')))
 app.get('/carrito',(req,res) => res.sendFile(path.join(__dirname,'views','cart.html')))
-app.get('/logeo',(req,res) => res.sendFile(path.join(__dirname,'views','login.html')))
-app.get('/registro',(req,res) => res.sendFile(path.join(__dirname,'views','register.html')))
+app.get('/login',(req,res) => res.sendFile(path.join(__dirname,'views','login.html')))
+app.get('/register',(req,res) => res.sendFile(path.join(__dirname,'views','register.html')))
 
 
 app.listen(port,() => console.log('Servidor funcionando en el puerto ' + port))
