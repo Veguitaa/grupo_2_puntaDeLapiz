2f17c04231152c5159b492f9940a1370548dc7d1
Veguitaa <webav2020@gmail.com>
2022-01-07 17:59:14 -0300

    home responsive + carousel

---
+++ site/public/js/carrusel.js
@@ -0,0 +1,34 @@
+window.addEventListener('load', function(){
+    new Glider(document.querySelector('.carousel__lista'), {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        draggable: true,
+        dots: '.carousel__indicadores',
+        arrows: {
+            prev: '.carousel__anterior',
+            next: '.carousel__siguiente'
+  },
+  responsive: [
+    {
+      // screens greater than >= 775px
+      breakpoint: 450,
+      settings: {
+        // Set to `auto` and provide item width to adjust to viewport
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        
+      }
+    },{
+      // screens greater than >= 1024px
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 7,
+        slidesToScroll: 1,
+        
+      }
+    }
+  ]
+
+
+    });
+})
